5bb17c98110dd48032062ccb04fdbfe746f74400
SunnatDevPy <stalker.yoqubov@gmail.com>
2025-02-21 13:45:56 +0500

    hello

--- fast_routers/cart.py
+++ fast_routers/cart.py
@@ -83,8 +83,11 @@ async def list_category_shop(client_id: int,
         if shop:
             if tip:
                 if product:
-                    cart = await Cart.create(bot_user_id=user.id, product_id=product_id, count=count,
-                                             shop_id=shop_id, tip_id=tip_id, total=tip.price * count)
+                    if cart:
+                        await Cart.update(cart.id, count=count, tip_id=tip_id, total=tip.price * count)
+                    else:
+                        cart = await Cart.create(bot_user_id=user.id, product_id=product_id, count=count,
+                                                 shop_id=shop_id, tip_id=tip_id, total=tip.price * count)
                     return {"ok": True, "cart": cart}
                 else:
                     return Response("Product topilmadi", status.HTTP_404_NOT_FOUND)
